8671f3363370e04a38989805781c21d4ef7b9cae
choijeonggyu <choi.jeonggyu@gmail.com>
2018-10-21 17:34:34 +0900

    kaist 연락처 만들기 수업-미션 1

--- class_5th_making_yellowbook/Mission1_contact_class.py
+++ class_5th_making_yellowbook/Mission1_contact_class.py
@@ -7,23 +7,33 @@ book=[]
 def add():
     con = contact()
     print("추가할 연락처를 입력하세요.")
-    print("name: " )
+    print("name: " , end ="")
     con.name = input()
-    print("number: ")
+    print("number: " , end ="")
     con.number = input()
     book.append(con)
 
-
 def delete():
     print("삭제할 이름을 입력하세요.")
-    print("name: ")
+    print("name: ", end='')
+    name = input()
 
+    length = len(book)
+    for i in range(0, length):
+        if book[i].name == name:
+            del book[i]
     print(" 연락처가 삭제 되엇습니다.")
+
 def printbook():
+    length = len(book)
+    for i in range (0,length):
+        print("name : ", book[i].name)
+        print("number : ", book[i].number)
 
+add()
+add()
+add()
 
-add()
-add()
-add()
 delete()
 printbook()
+
